1f65c752f4f0913188fc27e94355d8b80562361e
semoleg1986 <semoleg1986@gmail.com>
2024-01-29 19:53:36 +0400

    feat: impl. loader typescript

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 module.exports = (env) =>{
     return {
         mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: "[name].[contenthash].js",
@@ -15,5 +15,21 @@ module.exports = (env) =>{
             new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html')}),
             new webpack.ProgressPlugin()
         ],
+        module: {
+          rules: [
+            {
+              // Регулярное выражение для проверки расширения файла (.tsx)
+              test: /\.tsx?$/,
+              // Использование 'ts-loader' для обработки TypeScript файлов
+              use: 'ts-loader',
+              // Исключение из обработки файлов из директории 'node_modules'
+              exclude: /node_modules/,
+            },
+          ],
+        },
+      // Конфигурация разрешения файлов с указанием поддерживаемых расширений
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
     }
 }
