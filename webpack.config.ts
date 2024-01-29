80d60c6e30311fcabffd9b58b4e468abe6417e55
semoleg1986 <semoleg1986@gmail.com>
2024-01-29 20:55:57 +0400

    feat: config Webpack in TS

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack'
+
+type Mode = 'production' | "development";
+
+interface EnvVariables {
+  mode: Mode
+}
+
+export default (env: EnvVariables) =>{
+  const config: webpack.Configuration ={
+      mode: env.mode ?? 'development',
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      output: {
+          path: path.resolve(__dirname, 'build'),
+          filename: "[name].[contenthash].js",
+          clean: true
+      },
+      plugins: [
+          new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html')}),
+          new webpack.ProgressPlugin()
+      ],
+      module: {
+        rules: [
+          {
+            // Регулярное выражение для проверки расширения файла (.tsx)
+            test: /\.tsx?$/,
+            // Использование 'ts-loader' для обработки TypeScript файлов
+            use: 'ts-loader',
+            // Исключение из обработки файлов из директории 'node_modules'
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    // Конфигурация разрешения файлов с указанием поддерживаемых расширений
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+    }
+    return config
+}
